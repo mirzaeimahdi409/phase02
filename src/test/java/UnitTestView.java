f36dd0f2cf5d65e29959955669cfa40874458b7c
mirzaeimahdi409 <mirzaeimahdi409@gmail.com>
2022-08-13 11:59:01 +0430

    add UnitTest

--- src/test/java/UnitTestView.java
+++ src/test/java/UnitTestView.java
@@ -1,31 +1,23 @@
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import controller.Controller;
 import model.Employee;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
-public class UnitTest {
-    @Test
-    public void readFromJson() {
-        int result = Controller.controller.readFromJson();
-        if (result == 1) {
-            assertNotNull(Employee.getEmployees());
-        } else {
-            assertNull(Employee.getEmployees());
-        }
-    }
+import view.View;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+
+public class UnitTestView {
     @Test
-    public void writeToXml() throws IOException {
-        Controller.controller.readFromJson();
+    public void testView() throws IOException, ParseException {
+
+        Main.main(null);
+        assertNotNull(Employee.getEmployees());
         File file = new File("src/main/resources/EmployeeDb.xml");
         XmlMapper xmlMapper = new XmlMapper();
         ArrayList<Employee> values = xmlMapper.readValue(file, new TypeReference<>() {
